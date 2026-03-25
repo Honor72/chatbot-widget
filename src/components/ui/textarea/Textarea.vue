eb1a8d98b3025c40d9f74d5c6773cb27bf347273
Honor72 <win.honor72@gmail.com>
2026-03-25 08:58:43 +0100

    textarea length & bugfixes

--- src/components/ui/textarea/Textarea.vue
+++ src/components/ui/textarea/Textarea.vue
@@ -2,6 +2,7 @@
 import type { HTMLAttributes } from "vue";
 import { cn } from "@/lib/utils";
 import { useVModel } from "@vueuse/core";
+import { useAttrs } from "vue";
 
 const props = defineProps<{
 	class?: HTMLAttributes["class"];
@@ -13,6 +14,7 @@ const emits = defineEmits<{
 	(e: "update:modelValue", payload: string | number): void;
 }>();
 
+const attrs = useAttrs();
 const modelValue = useVModel(props, "modelValue", emits, {
 	passive: true,
 	defaultValue: props.defaultValue,
@@ -20,5 +22,9 @@ const modelValue = useVModel(props, "modelValue", emits, {
 </script>
 
 <template>
-	<textarea v-model="modelValue" :class="cn('flex w-full bg-transparent p-1 text-sm outline-none duration-200 placeholder:text-sm placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:placeholder:text-neutral-400 ', props.class)" />
+	<textarea
+		v-bind="attrs"
+		v-model="modelValue"
+		:class="cn('flex w-full bg-transparent p-1 text-sm outline-none duration-200 placeholder:text-sm placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:placeholder:text-neutral-400 ', attrs.class, props.class)"
+	/>
 </template>
